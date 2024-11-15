a964c8d16d347cc93155ee07935fe3a036cee45d
hady912012@gmail.com <hady912012@gmail.com>
2024-11-16 01:49:23 +0200

    dist backend

---
+++ src/migrations/1731712390430-firstmigrations.d.ts
@@ -0,0 +1,6 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+export declare class Firstmigrations1731712390430 implements MigrationInterface {
+    name: string;
+    up(queryRunner: QueryRunner): Promise<void>;
+    down(queryRunner: QueryRunner): Promise<void>;
+}
